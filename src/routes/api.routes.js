ebbc6f88c92b0028c3b1f048f40c681425e374e6
sirhcguti16-lab <sirhcguti16@gmail.com>
2026-07-19 23:36:48 -0500

    Refactor API routes to include new endpoints

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,27 +1,48 @@
 import { Router } from 'express';
 import { verificarToken, verificarRol } from '../middlewares/authMiddleware.js';
 
-import { getDuenos, registrarDueno, getCitas } from '../controladores/recepcionCtrl.js';
+import { 
+    getDuenos, registrarDueno, getCitas, getVeterinarios, 
+    getMascotas, agendarCita, editarDueno, editarMascota 
+} from '../controladores/recepcionCtrl.js';
+
 import { getCitasPendientes, atenderCita } from '../controladores/veterinarioCtrl.js';
-import { getMedicamentos, actualizarStock, getRecetasPendientes } from '../controladores/farmaciaCtrl.js';
+
+import { 
+    getMedicamentos, actualizarStock, editarMedicamento, 
+    getRecetasPendientes, despacharReceta 
+} from '../controladores/farmaciaCtrl.js';
+
+import { crearReceta } from '../controladores/recetasCtrl.js';
 
 const router = Router();
 
-// Todo lo que esté en este archivo requerirá token
 router.use(verificarToken);
 
 // --- RECEPCIÓN ---
 router.get('/recepcion/duenos', verificarRol(['recepcion']), getDuenos);
 router.post('/recepcion/duenos', verificarRol(['recepcion']), registrarDueno);
+router.put('/recepcion/duenos/:id', verificarRol(['recepcion']), editarDueno);
+
+router.get('/recepcion/mascotas', verificarRol(['recepcion']), getMascotas);
+router.put('/recepcion/mascotas/:id', verificarRol(['recepcion']), editarMascota);
+
 router.get('/recepcion/citas', verificarRol(['recepcion', 'veterinario']), getCitas);
+router.post('/recepcion/citas', verificarRol(['recepcion']), agendarCita);
+
+router.get('/recepcion/veterinarios', verificarRol(['recepcion']), getVeterinarios);
 
 // --- VETERINARIO ---
 router.get('/veterinario/citas', verificarRol(['veterinario']), getCitasPendientes);
 router.put('/veterinario/citas/:id', verificarRol(['veterinario']), atenderCita);
+router.post('/recetas', verificarRol(['veterinario']), crearReceta);
 
 // --- FARMACIA ---
 router.get('/farmacia/medicamentos', verificarRol(['farmacia', 'veterinario']), getMedicamentos);
-router.put('/farmacia/medicamentos/:id', verificarRol(['farmacia']), actualizarStock);
+router.put('/farmacia/medicamentos/:id', verificarRol(['farmacia']), editarMedicamento);
+router.put('/farmacia/medicamentos/stock/:id', verificarRol(['farmacia']), actualizarStock);
+
 router.get('/farmacia/recetas', verificarRol(['farmacia']), getRecetasPendientes);
+router.put('/farmacia/recetas/despachar/:id', verificarRol(['farmacia']), despacharReceta);
 
 export default router;
