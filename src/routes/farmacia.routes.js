ea21846e17312559d007d0531f6deab001979aed
sirhcguti16-lab <sirhcguti16@gmail.com>
2026-07-20 13:00:12 -0500

    Add routes for despachar and get recetas despachadas

--- src/routes/farmacia.routes.js
+++ src/routes/farmacia.routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
-import { getMedicamentos, actualizarStock, getRecetasPendientes } from '../controladores/farmaciaCtrl.js';
+import { 
+    getMedicamentos, 
+    actualizarStock, 
+    getRecetasPendientes, 
+    getRecetasDespachadas, 
+    despacharReceta 
+} from '../controladores/farmaciaCtrl.js'; 
 import { verificarToken, verificarRol } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/medicamentos', verificarToken, verificarRol(['farmacia', 'veterinario']), getMedicamentos);
 router.put('/medicamentos/:id', verificarToken, verificarRol(['farmacia']), actualizarStock);
-router.get('/recetas', verificarToken, verificarRol(['farmacia']), getRecetasPendientes);
+
+router.get('/recetas/pendientes', verificarToken, verificarRol(['farmacia']), getRecetasPendientes);
+
+router.get('/recetas/despachadas', verificarToken, verificarRol(['farmacia']), getRecetasDespachadas);
+router.put('/recetas/despachar/:id', verificarToken, verificarRol(['farmacia']), despacharReceta);
 
 export default router;
