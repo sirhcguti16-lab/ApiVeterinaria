069ce78ba066e397f066ac00a99818fe48374213
sirhcguti16-lab <sirhcguti16@gmail.com>
2026-07-21 10:13:45 -0500

    Add crearMedicamento and getRecetaPorId routes

--- src/routes/farmacia.routes.js
+++ src/routes/farmacia.routes.js
@@ -1,21 +1,24 @@
 import { Router } from 'express';
 import { 
     getMedicamentos, 
+    crearMedicamento,
     actualizarStock, 
     getRecetasPendientes, 
     getRecetasDespachadas, 
-    despacharReceta 
+    despacharReceta,
+    getRecetaPorId
 } from '../controladores/farmaciaCtrl.js'; 
 import { verificarToken, verificarRol } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/medicamentos', verificarToken, verificarRol(['farmacia', 'veterinario']), getMedicamentos);
+router.post('/medicamentos', verificarToken, verificarRol(['farmacia']), crearMedicamento);
 router.put('/medicamentos/:id', verificarToken, verificarRol(['farmacia']), actualizarStock);
 
 router.get('/recetas/pendientes', verificarToken, verificarRol(['farmacia']), getRecetasPendientes);
-
 router.get('/recetas/despachadas', verificarToken, verificarRol(['farmacia']), getRecetasDespachadas);
 router.put('/recetas/despachar/:id', verificarToken, verificarRol(['farmacia']), despacharReceta);
+router.get('/recetas/:id', verificarToken, verificarRol(['farmacia', 'veterinario']), getRecetaPorId);
 
 export default router;
